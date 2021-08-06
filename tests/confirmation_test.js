c41c04bb4c05674ae5f888d59e352ba19bb657c1
Vladyslav Palamarchuk <palamarchuk.gg@gmail.com>
2021-08-06 19:18:59 +0300

    more fixes

--- tests/confirmation_test.js
+++ tests/confirmation_test.js
@@ -5,12 +5,10 @@ Scenario('Verification of confirmation events', async ({ I }) => {
     let confirmations = await I.executeScript(async () => {
         let confirmationsArray = [];
         evolv.context.remoteContext.confirmations.forEach(element => {
-            console.log(element.cid)
             confirmationsArray.push(element.cid);
         })
         return confirmationsArray;
        
     });
-    console.log(confirmations)
     I.assertNotEqual(confirmations.length, 0);
 });
