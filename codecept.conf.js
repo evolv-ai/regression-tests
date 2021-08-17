2f9ffe4ca08bdeebdbdf47ed407571360de9e8e0
Vladyslav Palamarchuk <palamarchuk.gg@gmail.com>
2021-08-17 16:40:15 +0300

    fixes

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,7 +5,7 @@ exports.config = {
   helpers: {
     Puppeteer: {
       url: config.get('HOST_URL'),
-      show: config.get('SHOW_BROWSER'),
+      show: false,
       windowSize: '1200x900'
     },
     "ChaiWrapper" : {
