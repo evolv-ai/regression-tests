266d8bb133b70be4b371502b1361de3661b9a1b0
joshua-barnard-evolv <78709720+joshua-barnard-evolv@users.noreply.github.com>
2021-11-29 13:54:44 +1000

    fix: updated entry page tests

--- tests/entry_page_test.js
+++ tests/entry_page_test.js
@@ -15,7 +15,7 @@ async function arrangeTest(I, page) {
             })
             return confirmationsArray;
         } else {
-            throw new Error("No confirmations on this page!");
+            return[];
         };
     });
 
@@ -28,96 +28,97 @@ async function arrangeTest(I, page) {
     let experimentsConfig = await utils.getConfiguration();
     const allActiveKeysEntryPointInfo = await utils.getEntryPoints(activeKeys, experimentsConfig);
 
+    const countOfActiveKeys = activeKeys.length;
     const countOfEntryPoints = allActiveKeysEntryPointInfo.filter(key => key.isEntryPoint).length;
     const countOfConfirmations = confirmations.length;
 
+    console.log(`Active keys: ${countOfActiveKeys}`);
     console.log(`Entry points: ${countOfEntryPoints}`);
+    allActiveKeysEntryPointInfo.forEach(key => {
+        console.log(`${key.id} | isEntryPoint: ${key.isEntryPoint}`);
+    })
     console.log(`Confirmations: ${countOfConfirmations}`);
 
     const experimentDetails = {
         confirmations: confirmations,
         allActiveKeysEntryPointInfo: allActiveKeysEntryPointInfo,
+        countOfActiveKeys: countOfActiveKeys,
         countOfEntryPoints: countOfEntryPoints,
-        countOfConfirmations: countOfConfirmations
+        countOfConfirmations: countOfConfirmations,
     }
 
     return experimentDetails;
 }
 
 Scenario('Verify confirmation when both experiments match the context and the first has entry point equal to true', async ({ I }) => {
-    // Act
+    // ACT & ARRANGE
     const page = '/index.html';
     const exp = await arrangeTest(I, page);
 
-    // Assert
-    I.assertTrue(exp.countOfEntryPoints === exp.countOfConfirmations);
-    I.assertTrue(exp.countOfConfirmations > 0);
-
+    // ASSERT
+    // I expect there to be a confirmation for each experiment where entry point is true
+    I.assertTrue(exp.countOfConfirmations === exp.countOfEntryPoints);
+    // I expect there to be one confirmation
+    I.assertTrue(exp.countOfConfirmations === 1);
+    // I expect there to be no confirmation where entry point is false
     exp.confirmations.forEach(confirmation => {
         exp.allActiveKeysEntryPointInfo.find(key => {
             if (key.isEntryPoint) {
-                I.assertTrue(confirmation.indexOf(key.id) > -1);
+                I.assertTrue(confirmation.indexOf(key.id) > -1); // Confirmation
             } else {
-                I.assertTrue(confirmation.indexOf(key.id) === -1);
+                I.assertTrue(confirmation.indexOf(key.id) === -1); // No confirmation
             }
         })
     })
 });
 
-// Scenario('Verify confirmation when both experiments match the context and the second has entry point equal to true', async ({ I }) => {
-//     //Act
-//     const page = '/index.html';
-//     await arrangeTest(I, page);
-//
-// });
+Scenario('Verify no confirmations when EXPERIMENT 1 entry page is false', async ({ I }) => {
+    // ACT & ARRANGE
+    const page = '/clone.html';
+    const exp = await arrangeTest(I, page);
 
-// Scenario('Verify confirmation when both experiments match the context and both have entry point equal to true', async ({ I }) => {
-//     //Act
-//     const page = '/index.html';
-//     await doTest(I, page);
-//     I.assertTrue(countOfConfirmations > 0);
-// });
-//
-// Scenario('Verify no confirmations when none of experiment matching', async ({ I }) => {
-//     //Act
-//     I.amOnPage('/');
-//     I.wait(2);
-//     let confirmations = await I.executeScript(async () => {
-//         let confirmationsArray = [];
-//
-//         if(evolv.context.remoteContext.experiments.confirmations){
-//
-//             evolv.context.remoteContext.experiments.confirmations.forEach(element => {
-//                 confirmationsArray.push(element.cid);
-//             })
-//             return confirmationsArray;
-//         }
-//         else {
-//             return [];
-//         };
-//     });
-//
-//     let activeKeys = await I.executeScript(async () => {
-//         let keys = await evolv.client.getActiveKeys();
-//         let filteredKeys = keys.current.filter(key => key.split('.').length < 3);
-//         return filteredKeys.map(key => key.split('.')[1]);
-//     })
-//
-//     let experimentsConfig = await utils.getConfiguration();
-//
-//     const entryPoints = await utils.getEntryPoints(activeKeys, experimentsConfig);
-//
-//     //Assert
-//     entryPoints.forEach(element => {
-//         if(element.isEntryPoint){
-//             I.assertContain(confirmations[entryPoints.indexOf(element)], element.id);
-//         }else{
-//             I.assertNotContain(confirmations[entryPoints.indexOf(element)], element.id);
-//         }
-//     });
-//
-//     I.assertEqual(confirmations.length, 0);
-//     entryPoints.forEach(element => {
-//         I.assertEqual(element.isEntryPoint, false);
-//     });
-// });
+    // ASSERT
+    // I expect the entry point for the active key to be false
+    exp.allActiveKeysEntryPointInfo.forEach(key => {
+        I.assertTrue(key.isEntryPoint === false);
+    })
+    // I expect there to be no confirmations
+    I.assertTrue(exp.countOfConfirmations === 0);
+
+});
+
+Scenario('Verify confirmation when EXPERIMENT 2 entry page is true', async ({ I }) => {
+    // ACT & ARRANGE
+    const page = '/clone1.html';
+    const exp = await arrangeTest(I, page);
+
+    // ASSERT
+    // I expect the entry point for the active key to be true
+    exp.allActiveKeysEntryPointInfo.forEach(key => {
+        I.assertTrue(key.isEntryPoint === true);
+    })
+    // I expect there to be one confirmation
+    I.assertTrue(exp.countOfConfirmations === 1);
+});
+
+Scenario('Verify no confirmations when none of the experiments match', async ({ I }) => {
+    /*
+    There are no experiments running on page '/'.
+    This test should be checking for active experiments that don't match the current context. Otherwise, we're not testing anything.
+     */
+    // ACT & ARRANGE
+    const page = '/';
+    const exp = await arrangeTest(I, page);
+
+    // ASSERT
+    // I expect there to be no confirmations
+    I.assertEqual(exp.countOfConfirmations, 0);
+    // I expect there to be no entry points equal to true
+    exp.allActiveKeysEntryPointInfo.forEach(key => {
+        I.assertEqual(key.isEntryPoint, false); // This won't execute until there are active experiments
+    });
+    // Another way of checking the above statement, except above is more explicit
+    I.assertTrue(exp.countOfEntryPoints === 0);
+    // I expect there to be more than one entry point
+    I.assertTrue(exp.countOfEntryPoints > 1); // This will fail until there are active experiments
+});
